5d6ed3fab5ed2e660819b49b95eac62134babe6c
B. Ferrer <66350498+Messerschmidts101@users.noreply.github.com>
2024-09-08 00:57:08 +0800

    Adjusted llm component to latest

--- Large_Language_Model/LLM_Component.py
+++ Large_Language_Model/LLM_Component.py
@@ -2,6 +2,7 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import DirectoryLoader
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_community.vectorstores import Chroma
 
@@ -9,111 +10,131 @@ from langchain_community.vectorstores import Chroma
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import PromptTemplate
 
-#For LLM\
+#For LLM
+from langchain_community.llms import HuggingFaceEndpoint
 from langchain_groq import ChatGroq
+from langchain_openai import AzureChatOpenAI
 
 import os
 import uuid
 from datetime import datetime
 import time
-#from LLM_Model.LLM_Context_And_Rules import Personas
 
 class LLM:
-    '''
-    Requires the following:
-        1. intLLMSetting
-        2. strAPIKey 
-        3. strInjestPath
-        4. strPromptTemplate (Optional)
-        5. boolCreateDatabase (Optional)
-    '''
-    error_list = ['langchain_core.messages.ai.AIMessage', 'RateLimitError']
-    def __init__(self,intLLMSetting:int,
-                 strAPIKey:str,
-                 strInjestPath:str,
-                 strPromptTemplate:str,
-                 boolCreateDatabase:bool = False):
+    def __init__(self, intLLMSetting, 
+                 strIngestPath, 
+                 strPromptTemplate, 
+                 strAPIKey = None, 
+                 boolCreateDatabase=False, 
+                 fltTemperature = 0.1, 
+                 intRetrieverK = 5):
         self.boolCreateDatabase = boolCreateDatabase
-        self.objEmbedding = self.ingest_database(strInjestPath)
         self.strAPIKey = strAPIKey
         self.strPromptTemplate = strPromptTemplate
-        if intLLMSetting == 1:
-            #Step 1: Load LLM
-            self.objLLM = ChatGroq(temperature=.1, model_name="mixtral-8x7b-32768", groq_api_key=self.strAPIKey)
-            #Step 2: Load Template
-            self.objPromptTemplate = PromptTemplate(
-                template = self.strPromptTemplate,
-                input_variables=["context", "question"]
+        self.objEmbedding = self.ingest_database(strIngestPath)
+        self.initialize_llm(intLLMSetting,fltTemperature,intRetrieverK)
+
+    def initialize_llm(self, intLLMSetting, fltTemperature, intRetrieverK):
+        '''
+        This method creates LLM with their RAG Chain for this class
+        '''
+        dictLLMSettings = {
+            1: "mixtral-8x7b-32768",
+            2: "llama3-70b-8192",
+            3: "mistralai/Mistral-7B-Instruct-v0.2",
+            4: "gpt-4o-mini"
+        }
+        strModelName = dictLLMSettings.get(intLLMSetting, None)
+        if not strModelName:
+            raise ValueError(f"Invalid LLM setting: {intLLMSetting}")
+            #output list of llm settings
+        if intLLMSetting in [1, 2]: # Groq LLM
+            if not self.strAPIKey:
+                raise ValueError(f"Requires API Key, set value of 'strAPIKey'")
+            self.objLLM = ChatGroq(temperature=fltTemperature, 
+                                   model_name=strModelName, 
+                                   groq_api_key=self.strAPIKey)
+        elif intLLMSetting == 3:  # HuggingFace LLM
+            self.objLLM = HuggingFaceEndpoint(repo_id=strModelName, 
+                                              temperature=fltTemperature, 
+                                              token=os.environ["HUGGINGFACEHUB_API_TOKEN"])
+        elif intLLMSetting == 4: # OpenAI LLM
+            self.objLLM = AzureChatOpenAI(
+                api_key=os.environ["AZURE_OPENAI_API_KEY"],
+                deployment_name=strModelName,  # Use your specific deployment name
+                model=strModelName,  # Or another model you have deployed
+                temperature=fltTemperature,
+                api_version="2024-02-01"
             )
-            #Step 3: Load Chain
-            self.objRetriever = self.objEmbedding.as_retriever(search_kwargs={"k": 4}) 
-            self.objChain = ( {"context": self.objRetriever | self.combine_docs ,"question": RunnablePassthrough()} |
-                                self.objPromptTemplate |
-                                self.objLLM 
-                             )
-        if intLLMSetting == 2:
-            #Step 1: Load LLM
-            self.objLLM = ChatGroq(temperature=.1, model_name="llama3-70b-8192", groq_api_key=self.strAPIKey)
-            #Step 2: Load Template
-            self.objPromptTemplate = PromptTemplate(
-                template = self.strPromptTemplate,
-                input_variables=["context", "question"]
-            )
-            #Step 3: Load Chain
-            self.objRetriever = self.objEmbedding.as_retriever(search_kwargs={"k": 5}) 
-            self.objChain = ( {"context": self.objRetriever | self.combine_docs ,"question": RunnablePassthrough()} |
-                                self.objPromptTemplate |
-                                self.objLLM 
-                             )
-            
-    def combine_docs(self,docs):
+        self.objPromptTemplate = PromptTemplate(template=self.strPromptTemplate, 
+                                                input_variables=["context", "question"])
+        self.objRetriever = self.objEmbedding.as_retriever(search_kwargs={"k": intRetrieverK})
+        self.objChain = ({"context": self.objRetriever | self.combine_docs, "question": RunnablePassthrough()} | 
+                         self.objPromptTemplate | 
+                         self.objLLM)
+
+    def combine_docs(self, docs):
+        '''
+        This method is a sub process for ingesting database, triggered by ingest_database()
+        '''
         return "\n\n".join(doc.page_content for doc in docs)
 
-    def ingest_database(self,strInjestPath):
+    def ingest_database(self, strIngestPath):
+        '''
+        This method converts all files in a directory and outputs a Chroma database on the same location.
+        This can happen manually or automatically. 
+        You can use this method manually when there is a need to update the knowledge base after this class was insantiated.
+        You use this method automatically when insantiating this class and 'boolCreateDatabase is' set to True.
+        '''
         objEmbeddingModel = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        if self.boolCreateDatabase:
-            objLoader = DirectoryLoader(
-                strInjestPath,
-                glob = "**/*.txt",
-                loader_cls = TextLoader,
-                show_progress = False)
-            strRawDocuments = objLoader.load()
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-            strDocuments = text_splitter.split_documents(strRawDocuments)
-            db = Chroma.from_documents(strDocuments, objEmbeddingModel, persist_directory = strInjestPath + "/chroma_embeddings")
-            return db
-        else:
-            db = Chroma(embedding_function = objEmbeddingModel, persist_directory = strInjestPath + "/chroma_embeddings")
-            return db
-        
-    def get_response(self, strQuestion, strOutputPath = None):
-        # Retrieve relevant documents based on the question
-        objResponseRetriever = self.objRetriever.get_relevant_documents(strQuestion)
-        intMaxRetry = 3
-        intTry = 0
-        while intTry != intMaxRetry:
-            intTry = intTry + 1
-            try:
-                response = self.objChain.invoke(strQuestion)
-                response_content = response.content
-                break
-            except AttributeError as e:
-                response_content = self.objChain.invoke(strQuestion)
-                break
-            except Exception as e:
-                time.sleep(90)
-                print("Error on get_response(): ",e)
-        if strOutputPath:
-            current_datetime = datetime.now().strftime('%Y%m%d_%H%M%S')
-            unique_id = uuid.uuid4()
-            output_file_path = os.path.join(strOutputPath, f'LLMSetting_1_response_{current_datetime}_{unique_id}.txt')
-            with open(output_file_path, 'w', encoding='utf-8') as file:
-                file.write(response_content)
-        if strOutputPath:
-            current_datetime = datetime.now().strftime('%Y%m%d_%H%M%S')
-            unique_id = uuid.uuid4()
-            output_file_path = os.path.join(strOutputPath, f'LLMSetting_1_response_{current_datetime}_{unique_id}.txt')
-            with open(output_file_path, 'w', encoding='utf-8') as file:
-                file.write(response_content)
+        strKnowledgeDirectory = f"{strIngestPath}/chroma_embeddings"
 
-        return objResponseRetriever, response_content
+        if self.boolCreateDatabase:
+            objLoader = DirectoryLoader(strIngestPath, glob="**/*.txt", loader_cls=TextLoader, show_progress=False)
+            raw_documents = objLoader.load()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+            documents = text_splitter.split_documents(raw_documents)
+            return Chroma.from_documents(documents, objEmbeddingModel, persist_directory=strKnowledgeDirectory)
+        else:
+            return Chroma(embedding_function=objEmbeddingModel, persist_directory=strKnowledgeDirectory)
+
+    def get_response(self, strQuestion, 
+                     strOutputPath=None, 
+                     boolShowSource = False,
+                     intRetries = 3,
+                     intDelay = 90,
+                     boolVerbose = False):
+        if intDelay < 90:
+            print('Warning: intDelay is less than 90, setting intDelay to 90 or higher.')
+            intDelay = 90
+        if boolShowSource:
+            objResponseRetriever = self.objRetriever.get_relevant_documents(strQuestion)
+        else:
+            objResponseRetriever = None
+        strResponse = self.retry_chain_invoke(strQuestion,intRetries,intDelay,boolVerbose)
+        if strOutputPath:
+            self.save_response(strResponse, strOutputPath)
+        return strResponse,objResponseRetriever
+
+    def retry_chain_invoke(self, strQuestion, intRetries, intDelay, boolVerbose):
+        for intAttempt in range(intRetries):
+            try:
+                strResponse = self.objChain.invoke(strQuestion)
+                return strResponse.content
+            except AttributeError:
+                return self.objChain.invoke(strQuestion)
+            except Exception as e:
+                if intAttempt < intRetries - 1:
+                    if boolVerbose:
+                        print(f'Error {intAttempt}: {e}. Retrying in {intDelay} seconds')
+                    time.sleep(intDelay)
+                else:
+                    raise RuntimeError(f"Failed after {intRetries} attempts: {e}")
+
+    def save_response(self, strResponse, strOutputPath):
+        current_datetime = datetime.now().strftime('%Y%m%d_%H%M%S')
+        unique_id = uuid.uuid4()
+        output_file_path = os.path.join(strOutputPath, f'LLM_response_{current_datetime}_{unique_id}.txt')
+        
+        with open(output_file_path, 'w', encoding='utf-8') as file:
+            file.write(strResponse)
--- test.py
+++ test.py
@@ -5,10 +5,13 @@ import Large_Language_Model.LLM_Component as LLM_Component
 import Large_Language_Model.Personas as Personas
 strPromptTemplate = Personas.strPersonaUWU + Personas.strDefaultConversationTemplate 
 
-
-objLLM = LLM_Component.LLM(1,os.getenv('GROQ_KEY'),'Knowledge_Base',strPromptTemplate,True)
-strResponse = objLLM.get_response('Hi what is your name')[1]
+objLLM = LLM_Component.LLM(intLLMSetting = 1,
+                           strIngestPath = 'Knowledge_Base',
+                           strPromptTemplate = strPromptTemplate,
+                           strAPIKey = os.getenv('GROQ_KEY'),
+                           boolCreateDatabase = True)
+strResponse = objLLM.get_response('Hi what is your name',boolVerbose=True)[0]
 print(strResponse)
 
-strResponse = objLLM.get_response('Explain retention process to me')[1]
+strResponse = objLLM.get_response('Show the email template',boolVerbose=True)[0]
 print(strResponse)
